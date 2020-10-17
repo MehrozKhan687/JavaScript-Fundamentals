c3c6f2226ffa275b058d9f31fe48977d5e351247
Mehroz Khan <73029015+MehrozKhan687@users.noreply.github.com>
2020-10-17 19:43:50 +0500

    Add files via upload

---
+++ Practice/mainPractice.js
@@ -0,0 +1,152 @@
+// https://dev.to/dip15739/responsive-website-with-only-1-css-property-3ea9?fbclid=IwAR075JUHpYjOVbsJNUbpz1ouNdhXK2HF1mRyUFSwO5QabdejrF7nuQt7FN8
+// clamp CSS
+
+
+// // var a = '10';
+// // var b = 15;
+
+// // function age(){
+// //     if (b === 16) {
+// //         console.log('he is eligible')
+// //     }else {
+// //         console.log('he is not eligible')
+// //     };
+// // };
+
+// // console.log(age());
+
+
+// // function age (a){
+// //     if ( a >= 25 || a <= 26) {
+// //         console.log(true)
+// //     }
+// //     else {
+// //         console.log(false)
+// //     };
+// // };    
+// // console.log(age(25));
+
+// // var week = 2
+// // switch (week) {
+// //     case 1:
+// //          console.log('today is monday')
+        
+// //         break;
+// //     case 2: 
+// // console.log('today is tuesday')
+// //         break;
+// //         default:console.log('error')
+// // }
+// // console.log(week)
+
+// var a = 'Nice bhund';
+// console.log(a.length);
+
+// var b = 'shayan Ullah'
+// console.log(b.includes('ullah'));
+
+// console.log(b.toUpperCase());
+// console.log(b.toLowerCase());
+
+// var value = 'Javascript';
+// console.log(value[0]);
+// console.log(value[1]);
+// console.log(value[2]);
+// console.log(value[3]);
+
+// console.log(value[value.length - 2]);
+
+// var name = 'Shayan'
+
+// console.log(name.substring(3));
+// console.log(name.substring(1,5));
+
+// var nameA = 'Alexendra'
+// var nameB = 'Daddario'
+
+// console.log(`Female Actor name is ${nameA} ${nameB}`)
+
+// var a = 10 
+// var b = 2 
+// var c = 1 
+// var d = a + 6 * c - b / (5 + b)
+// //            
+// // 0.2
+// console.log(d)
+
+
+
+function bhund(r,s){
+    return (r+s)
+}
+console.log(bhund(6,8))
+
+function add (W1, W2){
+var W3 = W1 + W2
+console.log('the output is ' + W3)
+}
+
+var W4 = add(2, 2)
+W4;
+
+function sub (a,b) {
+    return (a-b)
+}
+function divide(c,d){
+    return sub(c,d)/d
+}
+var total = divide (8,4)
+console.log(total)
+
+// var a = x => x + 2
+// console.log(a(4));
+
+// var c = (x, y) => x + y
+// console.log(c(2, 2))
+
+let w = a => a + 6
+console.log(w(12))
+
+var r = (a,b) => a+b
+console.log(r(4,4))
+
+
+let a = t  => t + 7
+console.log(a(7))
+
+
+var l = (w,b) => w+b
+console.log(l(9,9))
+
+let func = (renderdata) => {
+    setTimeout(() => {
+     renderdata('pussyeater')   
+    }, 20000);
+};
+
+function renderdata(x){
+console.log(x)
+};
+func(renderdata);
+
+
+
+let fuck =  (a , b) => {
+    return(a + b)
+}
+
+console.log(fuck(4,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
